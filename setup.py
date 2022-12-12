1024d11cdc0ebd988687e3b9ddae56088c42cc20
Charles Li <charles9000li@gmail.com>
2022-12-11 19:00:03 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="cgtools",
+    author="Charles Li",
+    author_email="charlesli@ucsb.edu",
+    description="Utilities for mapping to and modifying coarse-grained systems",
+    keywords="molecular dynamics, coarse graining",
+    packages=['cgtools'],
+    python_requires=">=3.6",
+    install_requires=[]
+)
